0043712858aa7a60e4a4f7d9bb71a5710cbe833e
rontinim <60421879+rontinim@users.noreply.github.com>
2026-01-03 15:20:02 +0100

    Update config.h

--- config.h
+++ config.h
@@ -1,8 +1,5 @@
 #pragma once
 
-// Questo file deve contenere SOLO default/valori locali.
-// Le stringhe principali (WIFI_*, MQTT_*, ZONE_NAME) preferibilmente arrivano da build_flags per-env.
-
 #ifndef WIFI_SSID
   #define WIFI_SSID "ssid"
 #endif
@@ -19,9 +16,6 @@
   #define MQTT_PORT 1883
 #endif
 
-// NON definire ZONE_NAME qui: lo settiamo in platformio.ini per env:bed1/env:bed2
-
-// Pin relay (se vuoi tenerli qui come default)
 #ifndef RELAY_LED_PIN
   #define RELAY_LED_PIN 5
 #endif
@@ -30,7 +24,6 @@
   #define RELAY_PUMP_PIN 21
 #endif
 
-// Se ti serve ancora questo flag, ok tenerlo
 #ifndef RELAY_ACTIVE_LOW
   #define RELAY_ACTIVE_LOW 1
 #endif
